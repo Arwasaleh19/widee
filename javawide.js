13fab5c7a3aedfca6f83182e1e0ffd20c9657ef2
Arwasaleh19 <152069192+Arwasaleh19@users.noreply.github.com>
2023-11-26 15:15:56 +0300

    Add files via upload

---
+++ javawide.js
@@ -0,0 +1,15 @@
+function validation(){
+
+var fne=document.getElementById("fphone");
+var fte=document.getElementById("ftext");
+var submit=document.getElementById("soso");
+var reset=document.getElementById("roro");
+
+if(fne.value === ""){
+	document.getElementById("f-alert").innerHTML="Error"
+	fne.focus();
+	return false
+
+}
+	
+}
